a178522107f2077e623287232e6dedefd1a05213
Victor Costa <21337578+Darckfast@users.noreply.github.com>
2025-02-08 10:15:49 -0300

    fix: fixed lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,31 @@
+local cmp = require('cmp')
+local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+    snippet = {
+        expand = function(args)
+            -- You need Neovim v0.10 to use vim.snippet
+            vim.snippet.expand(args.body)
+        end,
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Up>'] = cmp.mapping.select_prev_item(cmp_select_opts),
+        ['<Up>'] = cmp.mapping.select_prev_item(cmp_select_opts),
+        ['<C-Down>'] = cmp.mapping.select_next_item(cmp_select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(cmp_select_opts),
+        ['<C-p>'] = cmp.mapping.complete(),
+    },
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
@@ -7,15 +35,15 @@ require('mason-lspconfig').setup({
         'eslint',
         'lua_ls',
         'html',
-        'eslint',
         'cssls',
-        'tailwindcss',
-        'svelte',
-        'gopls'
+        'gopls',
+        'jsonls'
     },
     handlers = {
         function(server_name)
-            require('lspconfig')[server_name].setup({})
+            require('lspconfig')[server_name].setup({
+                capabilities = capabilities
+            })
         end,
     },
 })
@@ -39,32 +67,6 @@ lspconfig.gopls.setup({
     },
 })
 
-local cmp = require('cmp')
-local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
-
-cmp.setup({
-    sources = {
-        { name = 'nvim_lsp' },
-    },
-    snippet = {
-        expand = function(args)
-            -- You need Neovim v0.10 to use vim.snippet
-            vim.snippet.expand(args.body)
-        end,
-    },
-    mapping = {
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Up>'] = cmp.mapping.select_prev_item(cmp_select_opts),
-        ['<Up>'] = cmp.mapping.select_prev_item(cmp_select_opts),
-        ['<C-Down>'] = cmp.mapping.select_next_item(cmp_select_opts),
-        ['<Down>'] = cmp.mapping.select_next_item(cmp_select_opts),
-        ['<C-p>'] = cmp.mapping.complete()
-    },
-})
-
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
@@ -80,7 +82,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
             vim.api.nvim_create_autocmd("BufWritePre", {
                 buffer = args.buf,
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                    vim.lsp.buf.format({ bufnr = args.buf })
+                    -- vim.lsp.buf.format()
                 end,
             })
         end
--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -22,8 +22,11 @@ vim.keymap.set("i", '{', '{}<Left>')
 vim.keymap.set("i", '{<CR>', '{<CR>}<Esc>O')
 
 -- vim.keymap.set("n", '<leader>u', ':buffer #<CR>')
+vim.api.nvim_set_keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set('n', '<leader-l>', "<cmd>lua require('jdtls').organize_imports()<cr>")
+-- vim.keymap.set('n', '<leader>l', "<cmd>lua require('jdtls').organize_imports()<cr>")
+vim.keymap.set("n", "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 
 vim.api.nvim_create_user_command('W', 'w', {})
